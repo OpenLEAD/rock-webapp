cc37623fcbf892407cbc6954ffaac11b8aa2a652
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-10-08 10:49:08 -0300

    remove the 'tasks' resource in Tasks::API
    This removes the need to have tasks/tasks in all URLs

--- lib/rock/webapp/tasks/api.rb
+++ lib/rock/webapp/tasks/api.rb
@@ -33,7 +33,6 @@ module Rock
                     ws
                 end
     
-                resource :tasks do
                     desc "Lists all tasks that are currently reachable on the name services"
                     params do
                         optional :extended_info, type: Boolean, default: false
@@ -45,39 +44,39 @@ module Rock
                             Hash[task_names: Orocos.name_service.names]
                         end
                     end
-    
+
                     helpers do
                         def task_by_name(name_service, name)
                             Orocos.name_service.get "#{name_service}/#{name}"
                         rescue Orocos::NotFound
                             error! "cannot find #{name_service}/#{name} on the registered name services", 404
                         end
-    
+
                         def port_by_task_and_name(name_service, name, port_name)
                             task_by_name(name_service, name).port(port_name)
                         rescue Orocos::NotFound
                             error! "cannot find port #{port_name} on task #{name_service}/#{name}", 404
                         end
                     end
-    
+
                     desc "Lists information about a given task"
                     get ':name_service/:name' do
                         Hash[task: task_by_name(params[:name_service], params[:name]).to_h]
                     end
-    
+
                     desc "Lists all ports of the task"
                     get ':name_service/:name/ports' do
                         task = task_by_name(params[:name_service], params[:name])
                         ports = task.each_port.map(&:model)
                         Hash[ports: ports.map(&:to_h)]
                     end
-    
+
                     desc "returns information about the given port"
                     get ':name_service/:name/ports/:port_name' do
                         port = port_by_task_and_name(*params.values_at('name_service', 'name', 'port_name'))
                         Hash[port: port.model.to_h]
                     end
-    
+
                     desc 'read a sample on the given port and returns it'
                     params do
                         optional :timeout, type: Float, default: 2.0
@@ -86,7 +85,7 @@ module Rock
                     end
                     get ':name_service/:name/ports/:port_name/read' do
                         port = port_by_task_and_name(*params.values_at('name_service', 'name', 'port_name'))
-    
+
                         if !port.respond_to?(:reader)
                             error! "#{port.name} is an input port, cannot read"
                         end
@@ -95,7 +94,7 @@ module Rock
                             port = port.to_async.reader(init: true, pull: true)
                             count = params.fetch(:count, Float::INFINITY)
                             ws = API.stream_async_data_to_websocket(env, port, count)
-    
+
                             status, response = ws.rack_response
                             status status
                             response
@@ -133,7 +132,6 @@ module Rock
                             error! "port type mismatch", 406
                         end     
                     end
-                end
             end
         end
     end
